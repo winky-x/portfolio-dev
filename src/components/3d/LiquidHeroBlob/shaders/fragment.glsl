1c3d6c545b5906b6c3119df3488632fba4a4ac3b
Yuvraj <spitfire1official@gmail.com>
2025-08-12 07:46:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/3d/LiquidHeroBlob/shaders/fragment.glsl
+++ src/components/3d/LiquidHeroBlob/shaders/fragment.glsl
@@ -1,40 +1,20 @@
-uniform float uTime;
-uniform float uMetallic;
-uniform float uRoughness;
-uniform float uTransmission;
-uniform float uThickness;
-uniform float uIor;
-uniform float uChromaticAberration;
-uniform vec3 uLight;
-uniform vec3 uDark;
-uniform vec3 uPrimary;
-uniform vec3 uSecondary;
-uniform vec3 uAccent;
-
 varying vec3 v_position;
 varying vec3 v_normal;
 
-vec3 getEyeVector(mat4 modelViewMatrix, vec3 position) {
-  return normalize(vec3(modelViewMatrix * vec4(position, 1.0)));
-}
+uniform vec3 uPrimary;
+uniform vec3 uSecondary;
+uniform vec3 uAccent;
+uniform vec3 uLight;
+uniform vec3 uDark;
 
 void main() {
-  vec3 eyeVector = getEyeVector(modelviewMatrix, v_position);
-  vec3 worldNormal = normalize(mat3(modelMatrix[0].xyz, modelMatrix[1].xyz, modelMatrix[2].xyz) * v_normal);
+    vec3 normal = normalize(v_normal);
+    float lighting = dot(normal, normalize(vec3(-0.3, 0.5, 0.6)));
 
-  // Fresnel
-  float fresnel = dot(eyeVector, worldNormal);
-  fresnel = pow(fresnel, 2.0);
+    vec3 color = mix(uPrimary, uSecondary, (v_position.x + 1.0) * 0.5);
+    color = mix(color, uAccent, (v_position.y + 1.0) * 0.5);
 
-  // Iridescence
-  vec3 color = mix(uPrimary, uSecondary, fresnel);
-  color = mix(color, uAccent, pow(fresnel, 2.0));
-
-  // Fake PBR
-  vec3 lightDir = normalize(vec3(1.0, 1.0, 1.0));
-  float diffuse = max(0.0, dot(worldNormal, lightDir)) * 0.5 + 0.5;
-
-  vec3 finalColor = mix(uDark, color * uLight, diffuse);
-  
-  gl_FragColor = vec4(finalColor, 1.0);
+    vec3 final_color = mix(uDark, color, lighting);
+    
+    gl_FragColor = vec4(final_color, 1.0);
 }
